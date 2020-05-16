ce93d5f4d20befdf4d335f688f3e605d50f74753
Esteban1891 <esteban.delahoz15@gmail.com>
2020-05-16 00:29:59 -0500

    add exercices in python3 of hackerrank

---
+++ Simple_Array_Sum/Simple_Array_Sum.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+import os
+import sys
+
+
+def simpleArraySum(ar):
+
+    suma = 0
+    for i in ar:
+        suma += i
+    return suma
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
